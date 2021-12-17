e5a73ca68536d600f2f53d4f133bfd893094473e
ChethanN18 <91309059+ChethanN18@users.noreply.github.com>
2021-12-17 20:35:24 +0530

    Update uploadtonexus.groovy

--- vars/uploadtonexus.groovy
+++ vars/uploadtonexus.groovy
@@ -1,9 +1,9 @@
 def call(conf){
- def reponame = conf["reponame"]
- def groupId = pomfile.groupId
- def type = pomfile.packaging
- def artifactID = pomfile.artifactId
- def version = pomfile.version
- def pomfile = readMavenPom file: 'pom.xml'
-nexusArtifactUploader artifacts: [[artifactId: "${artifactID}", classifier: '', file: "target/${art}ifactID}-${version}.war", type: "${type}"]], credentialsId: 'nexus', groupId: "${groupId}", nexusUrl: '172.31.30.71:8081/', nexusVersion: 'nexus3', protocol: 'http', repository: "${reponame}", version: "${version}"
+  def repoName = conf["repoName"]
+  def pomfile = readMavenPom file: 'pom.xml'
+  def version = pomfile.version
+  def artifactID = pomfile.artifactId
+  def type = pomfile.packaging
+  def groupId = pomfile.groupId
+  nexusArtifactUploader artifacts: [[artifactId: "${artifactID}", classifier: '', file: "target/${artifactID}-${version}.war", type: "${type}"]], credentialsId: 'nexus', groupId: "${groupId}", nexusUrl: '172.31.30.71:8081/', nexusVersion: 'nexus3', protocol: 'http', repository: "${repoName}", version: "${version}"
 }
