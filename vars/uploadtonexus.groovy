365852b5ecfdab01b0d6c19bf95874ee5efa6cf3
ChethanN18 <91309059+ChethanN18@users.noreply.github.com>
2021-12-17 20:24:51 +0530

    Create uploadtonexus.groovy

---
+++ vars/uploadtonexus.groovy
@@ -0,0 +1,8 @@
+def call(reponame){
+ def groupId = pomfile.groupId
+ def type = pomfile.packaging
+ def artifactID = pomfile.artifactId
+ def version = pomfile.version
+ def pomfile = readMavenPom file: 'pom.xml'
+nexusArtifactUploader artifacts: [[artifactId: "${artifactID}", classifier: '', file: "target/${art}ifactID}-${version}.war", type: "${type}"]], credentialsId: 'nexus', groupId: "${groupId}", nexusUrl: '172.31.30.71:8081/', nexusVersion: 'nexus3', protocol: 'http', repository: "${reponame}", version: "${version}"
+}
