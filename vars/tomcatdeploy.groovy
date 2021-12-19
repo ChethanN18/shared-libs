666cb7ec945ae33235abe9892e92da8665598faf
ChethanN18 <91309059+ChethanN18@users.noreply.github.com>
2021-12-19 09:51:12 +0530

    Update tomcatdeploy.groovy

--- vars/tomcatdeploy.groovy
+++ vars/tomcatdeploy.groovy
@@ -1,7 +1,7 @@
 def call(ip,credsid,warname,version){
   sshagent(["${credsid}"]) {
-    sh "scp -o StrictHostKeyChecking=no target/${warname}-${version}.war ec2-user@${ip}:/opt/tomcat9/webapps/"
-    sh "ssh ec2-user@${ip} /opt/tomcat9/bin/shutdown.sh"
-    sh "ssh ec2-user@${ip} /opt/tomcat9/bin/startup.sh"
+    sh "scp -o StrictHostKeyChecking=no target/${warname}-${version}.war ec2-user@${ip}:/opt/tomcat8/webapps/"
+    sh "ssh ec2-user@${ip} /opt/tomcat8/bin/shutdown.sh"
+    sh "ssh ec2-user@${ip} /opt/tomcat8/bin/startup.sh"
   }
 }
